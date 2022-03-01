b991d1908070976d1a48e3b20b90b23a73ff045c
laughingclouds <zenithtaiga@gmail.com>
2022-03-01 19:23:32 +0530

    version for project

---
+++ version.hpp
@@ -0,0 +1,9 @@
+/* This defines the current version of the project
+"RELEASE" value inspired from here:
+https://en.wikipedia.org/wiki/Software_release_life_cycle
+*/
+#pragma once
+
+#define NAME "quiz-game"
+#define RELEASE "pre-alpha"
+#define VERSION "0.1.0"
