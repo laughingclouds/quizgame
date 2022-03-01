cfaef8bd09ca761d03a4c86c6251f548a9ef72ab
laughingclouds <zenithtaiga@gmail.com>
2022-03-01 19:16:47 +0530

    remove later

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+
+#include "version.hpp"
 using namespace std;
 
 int main() {
-    cout << "Hello lol" << endl;
+    cout << NAME << '\n';
+    cout << "Release: " << RELEASE << '\n';
+    cout << "Version: " << VERSION << endl;
+
+    return 0;
 }
