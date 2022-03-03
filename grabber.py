a7dadeb429824de6f3c8d818168ea1c58ce2130b
laughingclouds <zenithtaiga@gmail.com>
2022-03-03 13:38:10 +0530

    Made some Noice Changes
    I'm happy with this, and my soul has recovered from past trauma.

--- grabber.py
+++ grabber.py
@@ -1,15 +1,19 @@
-def join_str_path(base: str, *args: str) -> str:
-    str_path = base
-    for arg in args:
-        str_path += '/' + arg
-    return str_path
+"""
+Convenience script for recursively walking the source directory 
+and printing the paths of all the ``.cpp`` files.
+To be used by **meson.build** file.
+"""
+from os import walk as osWalk
+from os.path import join as joinPath
 
 def main():
     SRC_DIR = './quiz-game'
-    print(*[
-        join_str_path(SRC_DIR, 'quiz-game.cpp'),
-        join_str_path(SRC_DIR, 'utility', 'utility.cpp')
-    ])
+    for root, _, files in osWalk(SRC_DIR):
+        for fileName in files:
+            if fileName.endswith(".cpp"):
+                # end=" " is important! Because meson.build
+                # reads a space separated list of file names
+                print(joinPath(root, fileName), end=" ")
 
 if __name__ == "__main__":
     main()
