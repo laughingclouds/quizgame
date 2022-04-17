fd3cdb1f7116f326f0622a809fea8a0a681695df
laughingclouds <zenithtaiga@gmail.com>
2022-04-17 15:45:41 +0530

    .

--- controllers/StartGameController.h
+++ controllers/StartGameController.h
@@ -14,12 +14,13 @@ public:
   // METHOD_ADD(StartGameController::your_method_name, "/{1}/{2}/list", Get); //
   // path is /StartGameController/{arg1}/{arg2}/list
   ADD_METHOD_TO(StartGameController::startGameBasedOnCategoryId,
-             "/StartGame?category_id={1}", Get);
+                "/StartGame?category_id={1}", Get);
   METHOD_LIST_END
   /*
   your declaration of processing function maybe like this:
-  void get(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, int p1, std::string p2); 
-  void your_method_name(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback,
+  void get(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr
+  &)> &&callback, int p1, std::string p2); void your_method_name(const
+  HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback,
   double p1, int p2) const;
   */
 
--- main.cc
+++ main.cc
@@ -1,7 +1,3 @@
-#include <drogon/HttpAppFramework.h>
-#include <drogon/orm/DbClient.h>
-#include <drogon/orm/SqlBinder.h>
-#include <sqlite3.h>
 #include <drogon/drogon.h>
 
 #include "models/Category.h"
@@ -37,6 +33,5 @@ int main() {
   drogon::app().loadConfigFile("../config.json");
   // Run HTTP framework,the method will block in the internal event loop
   app().run();
-  // ::dbPtr = drogon::orm::DbClient::newSqlite3Client("main", 1);
   return 0;
 }
