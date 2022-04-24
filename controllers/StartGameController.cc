bc8df7cefc0fb8f0214d24db8945d13c1afbcc9c
laughingclouds <zenithtaiga@gmail.com>
2022-04-25 03:11:23 +0530

    It will now update user score in the database

--- controllers/StartGameController.cc
+++ controllers/StartGameController.cc
@@ -1,15 +1,41 @@
-#include "StartGameController.h"
+#include <drogon/HttpTypes.h>
+#include <drogon/orm/Row.h>
+#include <string>
+
 #include "../models/Category.h"
 #include "../models/Question.h"
+#include "StartGameController.h"
 
 #include <drogon/HttpAppFramework.h>
 #include <drogon/HttpResponse.h>
-#include <iostream>
+#include <drogon/orm/Mapper.h>
+#include <vector>
 
+#include "../exceptions/InvalidSession.hpp"
 #include "quizModels.hpp"
+#include "utils.hpp"
 
 using namespace drogon_model;
 using namespace drogon_model::sqlite3;
+
+void StartGameController::quizSetting(
+    const HttpRequestPtr &req,
+    std::function<void(const HttpResponsePtr &)> &&callback) const {
+
+  if (!isValidUserLoggedIn(req)) {
+    auto redir = HttpResponse::newRedirectionResponse("/Login", k303SeeOther);
+    callback(redir);
+  }
+  /** Get Category data from DB */
+  std::vector<Category> categoryVec =
+      orm::Mapper<Category>(app().getDbClient()).findAll();
+
+  HttpViewData data;
+  data.insert("categoryVec", categoryVec);
+
+  auto resp = HttpResponse::newHttpViewResponse("QuizSetting.csp", data);
+  callback(resp);
+}
 
 /**
 User is provided with a form, that has a number of questions and their options.
@@ -23,13 +49,13 @@ void StartGameController::startGameBasedOnCategoryId(
     int category_id) const {
 
   try {
+    isValidUserLoggedIn(req, true);
     if (category_id == 0)
       throw orm::UnexpectedRows("Given category ID is not accepted.");
 
     Category catObj = orm::Mapper<Category>(app().getDbClient())
                           .findByPrimaryKey(category_id);
-    // orm::Mapper<Question>(app().getDbClient()).findBy(orm::Criteria(orm::CompareOperator::EQ));
-    // vector<Question>
+
     auto ormQuestionVec =
         orm::Mapper<Question>(app().getDbClient())
             .findBy(orm::Criteria("categoryId", orm::CompareOperator::EQ,
@@ -40,6 +66,8 @@ void StartGameController::startGameBasedOnCategoryId(
     for (auto ormQuestion : ormQuestionVec)
       questions.push_back(quiz::Question(ormQuestion));
 
+    // shuffle the quiz::Question objects in a random order
+    std::shuffle(questions.begin(), questions.end(), randomEngine);
     HttpViewData data;
 
     data.insert("catObj", catObj);
@@ -58,20 +86,60 @@ void StartGameController::startGameBasedOnCategoryId(
     auto resp = HttpResponse::newRedirectionResponse("/QuizSetting",
                                                      k300MultipleChoices);
     callback(resp);
+
+  } catch (InvalidSessionException err) {
+
+    auto resp =
+        HttpResponse::newRedirectionResponse("/Login", drogon::k303SeeOther);
+    callback(resp);
   }
 }
 
 void StartGameController::gameSubmission(
     const HttpRequestPtr &req,
     std::function<void(const HttpResponsePtr &)> &&callback) const {
-  // auto resp =
-  //     HttpResponse::newRedirectionResponse("/QuizSetting", k300MultipleChoices);
-  // req->getBody()
+
+  int userId = std::stoi(req->getCookie("userId"));
+  int number_of_correctly_answered_questions = 0;
+  int number_of_questions = 0;
+
+  std::vector<quiz::Answer> answers;
+
   auto paramemters = req->getParameters();
   for (auto param : paramemters) {
-    std::cout << param.first << " " << param.second << '\n';
+    number_of_questions++;
+
+    auto question = orm::Mapper<Question>(app().getDbClient())
+                        .findByPrimaryKey(std::stoi(param.first));
+
+    answers.push_back(quiz::Answer(question));
+
+    if (question.getValueOfAnswerid() == std::stoi(param.second)) {
+      number_of_correctly_answered_questions++;
+
+      auto ormSolved = makeSolvedObject(question.getValueOfId(), userId);
+      orm::Mapper<Solved>(app().getDbClient()).insert(ormSolved);
+    }
   }
-  auto resp = HttpResponse::newHttpResponse();
-  resp->setBody(std::string(req->getBody()));
+
+  {
+    auto ormUser =
+        orm::Mapper<User>(app().getDbClient()).findByPrimaryKey(userId);
+    ormUser.setScore(ormUser.getValueOfScore() +
+                     number_of_correctly_answered_questions);
+  }
+
+  HttpViewData data;
+
+  data.insert("number_of_questions", number_of_questions);
+  data.insert("number_of_correctly_answered_questions",
+              number_of_correctly_answered_questions);
+  data.insert("answers", answers);
+
+  auto resp = HttpResponse::newHttpViewResponse("GameScore.csp", data);
+
+  resp->addCookie(makeSecureCookie(
+      "score", std::to_string(number_of_correctly_answered_questions)));
+
   callback(resp);
 }
