833510ff1c0264f6c8085ce1c703c026d7da1949
laughingclouds <zenithtaiga@gmail.com>
2022-04-25 03:02:16 +0530

    Removed what wasn't needed

--- main.cc
+++ main.cc
@@ -7,35 +7,12 @@
 
 auto randomEngine = std::default_random_engine();
 
-int main() {
-  using namespace drogon;
-  
+int main() {  
   std::cout << "Server started!\n";
   
-  app().registerHandler(
-      "/", [](const HttpRequestPtr &req,
-              std::function<void(const HttpResponsePtr &)> &&callback) {
-        auto resp = HttpResponse::newHttpViewResponse("quiz_game_home.csp");
-        callback(resp);
-      });
-
-  app().registerHandler(
-      "/list_para",
-      [=](const HttpRequestPtr &req,
-          std::function<void(const HttpResponsePtr &)> &&callback) {
-        auto para = req->getParameters();
-        HttpViewData data;
-        data.insert("title", "ListParameters");
-        data.insert("parameters", para);
-        auto resp =
-            HttpResponse::newHttpViewResponse("ListParameters.csp", data);
-        callback(resp);
-      });
-  // Set HTTP listener address and port
-  app().addListener("0.0.0.0", 8080);
-  // Load config file
   drogon::app().loadConfigFile("../config.json");
+
   // Run HTTP framework,the method will block in the internal event loop
-  app().run();
+  drogon::app().run();
   return 0;
 }
