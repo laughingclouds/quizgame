9cb5791d53708b984f4a7981b106bc4ae70aba62
laughingclouds <zenithtaiga@gmail.com>
2022-05-06 01:48:17 +0530

    Make controller that serves leaderboard It will query the database and insert the required data into the view

--- controllers/LeaderBoard/LeaderBoardController.cc
+++ controllers/LeaderBoard/LeaderBoardController.cc
@@ -1,13 +1,32 @@
 #include "LeaderBoardController.h"
+#include "../../models/models.hpp"
+
+using namespace drogon_model;
+using namespace drogon_model::sqlite3;
 
 /** Serve the `/LeaderBoard` page
 1. Query database for top scores
 2. Calculate top five scores
 3. Insert into view */
-void LeaderBoardController::leaderBoardHome(const HttpRequestPtr &req, std::function<void (const HttpResponsePtr &)> &&callback) const {
+void LeaderBoardController::leaderBoardHome(
+    const HttpRequestPtr &req,
+    std::function<void(const HttpResponsePtr &)> &&callback) const {
 
-    // Add code here
+  // orderBy: colum score, in descending
+  // limit: the search results to only 5
+  // offset: from where to start querying data from
+  // User "No Name" lies at row 0 of the database
+  auto ormUsersTopScorers =
+      orm::Mapper<User>(app().getDbClient())
+          .orderBy(User::Cols::_score, orm::SortOrder::DESC)
+          .limit(5)
+          .offset(1)
+          .findAll();
 
-    auto resp = HttpResponse::newHttpViewResponse("LeaderBoardHome");
-    callback(resp);
+  HttpViewData data;
+
+  data.insert("ormUsersTopScorers", ormUsersTopScorers);
+
+  auto resp = HttpResponse::newHttpViewResponse("LeaderBoardHome", data);
+  callback(resp);
 }
