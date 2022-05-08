6207af551e441c4e5439d7cb14cd5d057b54113c
laughingclouds <zenithtaiga@gmail.com>
2022-05-09 04:17:41 +0530

    .

--- controllers/Home/HomePageController.cc
+++ controllers/Home/HomePageController.cc
@@ -1,5 +1,6 @@
+#include <iostream>
+
 #include "HomePageController.h"
-#include <drogon/HttpViewData.h>
 
 #include "../utility/utils.hpp"
 
@@ -7,11 +8,14 @@ void HomePageController::homePage(
     const HttpRequestPtr &req,
     std::function<void(const HttpResponsePtr &)> &&callback) const {
 	
+  const std::string USER_NAME = req->getCookie("user");
 	bool IS_USER_LOGGED_IN = isValidUserLoggedIn(req);
 
 	HttpViewData data;
 
 	data.insert("IS_USER_LOGGED_IN", IS_USER_LOGGED_IN);
+  data.insert("USER_NAME", USER_NAME);
+
 	//
 	auto resp = HttpResponse::newHttpViewResponse("HomePage", data);
 	callback(resp);
