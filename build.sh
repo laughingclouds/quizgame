d07fcb66614eca21f6eff38270496c97eee80654
laughingclouds <zenithtaiga@gmail.com>
2022-07-16 14:08:53 +0530

    Build script for convenience

---
+++ build.sh
@@ -0,0 +1,20 @@
+PROJECT_ROOT=$(pwd)
+BUILD_PATH=$PROJECT_ROOT/build/
+
+mkdir -p build
+
+# BACKEND_BUILD_PATH=$PROJECT_ROOT/build/
+
+cd $PROJECT_ROOT/frontend/
+npm run build
+
+
+cd $PROJECT_ROOT/backend/
+
+echo "Copy all static files into $BUILD_PATH"
+cp static/media/* $BUILD_PATH/static/media/
+
+echo "Building backend into $BUILD_PATH"
+go build -o $BUILD_PATH
+
+echo "Done"
